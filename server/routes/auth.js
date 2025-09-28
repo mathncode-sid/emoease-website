807b03e30ebdb73c1b09eef06fc8f45c8d5ffa98
mathncode-sid <sidneybarakamuriuki1@gmail.com>
2025-09-28 23:50:22 +0300

    routing for the auth system

---
+++ server/routes/auth.js
@@ -0,0 +1,14 @@
+// Authentication routes
+const express = require("express")
+const router = express.Router()
+const authController = require("../controllers/authController")
+const { authenticateToken } = require("../middleware/auth")
+
+// Public routes
+router.post("/register", authController.register)
+router.post("/login", authController.login)
+
+// Protected routes
+router.get("/profile", authenticateToken, authController.getProfile)
+
+module.exports = router
